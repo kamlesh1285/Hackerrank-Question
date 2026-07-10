89ddd43ea99b0f85b70825473ede6aacba1575c5
kamlesh1285 <kamlesh1285ky@gmail.com>
2026-07-10 10:15:35 +0530

    first commit

---
+++ CPP/Basic Data Types.cpp
@@ -0,0 +1,21 @@
+// Basic Data Types
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int i;
+    long l;
+    char a;
+    float f;
+    double d;
+    cin>>i>>l>>a>>f>>d;
+    cout<<i<<endl;
+    cout<<l<<endl;
+    cout<<a<<endl;
+    cout<<f<<endl;
+    cout<<d<<endl;
+    return 0;
+}
